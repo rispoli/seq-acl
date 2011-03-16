97241428b0ab01ff4fe4cc246283a392ad6d6f75
Daniele Rispoli <daniele.rispoli@gmail.com>
2011-03-16 18:32:17 +0100

    deleted:    examples/test_defLIS2010.pl 		Useless example.

--- examples/test_defLIS2010.pl
+++
@@ -1,2 +0,0 @@
-?- latexify(([u], [], [], [u : '\\alpha' -> []('A', '\\alpha')]), 'examples/defLIS2010.fig.2.tex').
-?- latexify(([u], [], [], [u : []('A', ([]('A', '\\alpha') -> '\\alpha'))]), 'examples/defLIS2010.fig.3.tex').
