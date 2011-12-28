9fdd887b5b697289d5a6dec6c39186549a420740
Daniele Rispoli <daniele.rispoli@gmail.com>
2011-12-28 17:44:41 +0100

    renamed:    policies/df1.policy -> policies/df1.pl 	renamed:    remote_examples/policy -> remote_examples/policy.pl 	renamed:    remote_examples/policy_a -> remote_examples/policy_a.pl 		Policy files have suitable PoLicy extension. 	modified:   makefile 		+ GNU Prolog translation. 	new file:   gnu_prolog_toplevel.pl 		GNU Prolog binary toplevel. 	new file:   gnu_prolog_utilities.pl 		GNU Prolog utilities to bridge the gap with some SWI Prolog 		functions. 	modified:   server.cpp 	modified:   srsyncd.cpp 		+ GNU Prolog option and calls.

---
+++ gnu_prolog_utilities.pl
@@ -0,0 +1,75 @@
+filter(_, [], []) :- !.
+
+filter(P, [H | T], [H | R]) :-
+    call(P, H), !,
+    filter(P, T, R).
+
+filter(P, [_ | T], R) :-
+    filter(P, T, R).
+
+maplist(Goal, List) :-
+    maplist_(List, Goal).
+
+maplist_([], _).
+maplist_([Elem|Tail], Goal) :-
+    call(Goal, Elem),
+    maplist_(Tail, Goal).
+
+maplist(Goal, List1, List2) :-
+    maplist_(List1, List2, Goal).
+
+maplist_([], [], _).
+maplist_([Elem1|Tail1], [Elem2|Tail2], Goal) :-
+    call(Goal, Elem1, Elem2),
+    maplist_(Tail1, Tail2, Goal).
+
+flag(Sym, Old, NewExp) :-
+    g_read(Sym, Old),
+        nonvar(NewExp) ->
+            New is NewExp,
+            g_assign(Sym, New)
+            ;
+            g_read(Sym, New).
+
+gensym(Base, Atom) :-
+    atom_concat('gs_', Base, Key),
+    flag(Key, N, N+1),
+    number_atom(N, NA),
+    atom_concat(Base, NA, Atom), !.
+
+exclude(Goal, List, Included) :-
+    exclude_(List, Goal, Included).
+
+exclude_([], _, []).
+exclude_([X1|Xs1], P, Included) :-
+    (   call(P, X1)
+    ->  Included = Included1
+    ;   Included = [X1|Included1]
+    ),
+    exclude_(Xs1, P, Included1).
+
+partition(Pred, List, Included, Excluded) :-
+    partition_(List, Pred, Included, Excluded).
+
+partition_([], _, [], []).
+partition_([H|T], Pred, Incl, Excl) :-
+    (   call(Pred, H)
+    ->  Incl = [H|I],
+        partition_(T, Pred, I, Excl)
+    ;   Excl = [H|E],
+        partition_(T, Pred, Incl, E)
+    ).
+
+union([], L, L) :- !.
+union([H|T], L, R) :-
+    memberchk(H, L), !,
+    union(T, L, R).
+union([H|T], L, [H|R]) :-
+    union(T, L, R).
+
+subtract([], _, []) :- !.
+subtract([E|T], D, R) :-
+    memberchk(E, D), !,
+    subtract(T, D, R).
+subtract([H|T], D, [H|R]) :-
+    subtract(T, D, R).
