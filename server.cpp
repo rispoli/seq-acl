84db8eedc265eff460f8ef4e4758fcf45976c0e1
Daniele Rispoli <daniele.rispoli@gmail.com>
2012-05-01 23:32:56 +0200

    deleted:    UNLICENSE 		- Public Domain. 	new file:   COPYING 		+ AGPLv3.

--- server.cpp
+++ server.cpp
@@ -1,3 +1,24 @@
+/*
+
+   Copyright 2012 Daniele Rispoli, Valerio Genovese, Deepak Garg
+
+   This file is part of smart-rsync.
+
+   smart-rsync is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   smart-rsync is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public
+   License along with smart-rsync.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <argtable2.h>
 #include <errno.h>
 #include <fstream>
